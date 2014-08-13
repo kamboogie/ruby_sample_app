d540bf1fdfd3e3a9b80baa7e8e76d7397efbde98
Ezekiel <ez@vinple.com>
2014-08-13 23:34:55 +0000

    dynamic pages and deleted application.html.erb

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
-
+  render_views
+  
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to be_success
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+    
   end
 
   describe "GET contact" do
     it "returns http success" do
       get :contact
       expect(response).to be_success
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
@@ -21,6 +33,12 @@ RSpec.describe PagesController, :type => :controller do
       get :about
       expect(response).to be_success
     end
+     
+     it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+     
   end
   
 end
