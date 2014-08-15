64905f6b49bc0070627fac3dd7f7382d3b5f7f02
Ezekiel <ez@vinple.com>
2014-08-15 07:45:18 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  #get "users/new"
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',     via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/home',    to: 'pages#home',    via: 'get'
   match '/help',    to: 'pages#help',    via: 'get'
   match '/about',   to: 'pages#about',   via: 'get'
